d46c4d32951e50f8eae799195c58aed40d01e35a
balu-pinisetty <balupinisetty@gmail.com>
2021-05-26 21:50:00 +0530

    Finalising the Win or Tie

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -2,18 +2,26 @@
 echo "Flip Coin Simulation Problem"
 HEAD=0
 TAIL=0
-read -p "Enter the value to flip coins: " nmb
-for ((i=1; i<=$nmb; i++))
+while [[ $HEAD -lt 21 && $TAIL -lt 21 ]]
 do
 	face=$((RANDOM%2))
 	if [ $face -eq 1 ]
 	then
-#		echo "HEAD is WINNNER"
 		((++HEAD))
 	else
-#		echo "TAIL is WINNER"
 		((++TAIL))
 	fi
 done
-echo "HEAD won $HEAD times"
-echo "TAIL won $TAIL times"
+echo "HEAD faces $HEAD times"
+echo "TAIL faces $TAIL times"
+if [ $HEAD -gt $(($TAIL+2)) ]
+then
+	dif=$(($HEAD-$TAIL))
+	echo "Heads won by $dif times more than Tails"
+elif [ $TAIL -gt $(($HEAD+2)) ]
+then
+	dif=$(($TAIL-$HEAD))
+	echo "Tails won by $dif times more than Headss"
+else
+	echo "Its TIE since their difference is <=2"
+fi
