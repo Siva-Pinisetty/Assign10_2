dd1c02699f8ef0108037c3faf5b1cfd13ee6a063
balu-pinisetty <balupinisetty@gmail.com>
2021-05-26 22:45:12 +0530

    Extension to check win if tie comes

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -24,4 +24,31 @@ then
 	echo "Tails won by $dif times more than Headss"
 else
 	echo "Its TIE since their difference is <=2"
+	echo "By again flipping to make difference >2"
+	while [ 1 ]
+	do
+		face=$((RANDOM%2))
+		if [ $face -eq 1 ]
+		then
+			((++HEAD))
+		else
+			((++TAIL))
+		fi
+		dif1=$(($HEAD-$TAIL))
+		dif2=$(($TAIL-$HEAD))
+		if [[ $dif1 -gt 2 || $dif2 -gt 2 ]]
+		then
+			break;
+		fi
+	done
+	echo "HEAD faces $HEAD times"
+	echo "TAIL faces $TAIL times"
+	if [ $HEAD -gt $(($TAIL)) ]
+	then
+	dif=$(($HEAD-$TAIL))
+	echo "Heads won by $dif times more than Tails"
+	else
+	dif=$(($TAIL-$HEAD))
+	echo "Heads won by $dif times more than Tails"
+	fi
 fi
