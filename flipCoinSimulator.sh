31259e8429c1791a5d27af409b373897cf9a662a
balu-pinisetty <balupinisetty@gmail.com>
2021-05-26 21:12:05 +0530

    making Head and Tail count

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,19 @@
-#!/bin/bash -x
+#!/bin/bash
 echo "Flip Coin Simulation Problem"
-
-face=((RANDOM%2))
-if [ $face -eq 1 ]
-then
-	echo "HEAD is WINNNER"
-else
-	echo "TAIL is WINNER"
-fi
+HEAD=0
+TAIL=0
+read -p "Enter the value to flip coins: " nmb
+for ((i=1; i<=$nmb; i++))
+do
+	face=$((RANDOM%2))
+	if [ $face -eq 1 ]
+	then
+#		echo "HEAD is WINNNER"
+		((++HEAD))
+	else
+#		echo "TAIL is WINNER"
+		((++TAIL))
+	fi
+done
+echo "HEAD won $HEAD times"
+echo "TAIL won $TAIL times"
